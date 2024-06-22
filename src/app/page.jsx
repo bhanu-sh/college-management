0114ae0f4b012c6ca1a9cc83388651826ed5e270
bhanu-sh <bhanu1234sharma@gmail.com>
2024-06-22 22:01:09 +0530

    New Project setup

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="dark">
+      <div className="h-screen dark:bg-black dark:text-white">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
