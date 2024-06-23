78bbd610ced9c1159f84abfc49d4b160b8b89b6b
bhanu-sh <bhanu1234sharma@gmail.com>
2024-06-23 13:36:10 +0530

    dark mode switch and login screen added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,31 @@
 "use client";
+import { useAuth } from "@/contexts/authContext";
+import React from "react";
 
 export default function Home() {
+  const { darkMode, setDarkMode } = useAuth();
   return (
-    <div className="dark">
-      <div className="h-screen dark:bg-black dark:text-white">
-        <h1>Home</h1>
+    <div className={darkMode ? "dark" : ""}>
+      <div className="h-screen dark:bg-black">
+        <div className="flex flex-col mx-auto justify-center w-96 min-h-screen  dark:text-white">
+          <h1 className="text-4xl font-semibold text-center">Login</h1>
+
+          <label htmlFor="email">E-Mail</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            type="email"
+            id="email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            type="password"
+            id="password"
+          />
+          <button className="p-2 bg-blue-500 text-white rounded-lg w-full hover:bg-blue-600">
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/contexts/authContext.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext({
+  darkMode: false,
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        darkMode,
+        setDarkMode,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
